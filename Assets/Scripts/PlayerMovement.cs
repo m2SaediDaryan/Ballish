f3dd0e9c4cde6385355f0168a99eb1bccb4ab77d
m2SaediDaryan <m2saedidaryan@gmail.com>
2024-09-24 17:29:20 +0330

    fixed player goes center of screen

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,72 +7,81 @@ public class PlayerMovement : MonoBehaviour
     public float moveDirection;
     public int movementNumber;
     public GameManager gameManager;
+    public PanelManager panelManager;
     public bool right;
     public bool left;
     private Vector3 initialTouchPosition;
     private Vector3 initialPlayerPosition;
     public Setting setting;
+    private int nowCanReplayPlayerMovment;
 
     private void Start()
     {
         mainPosition = transform.position;
         setting = FindObjectOfType<Setting>();
         gameManager = FindObjectOfType<GameManager>();
+        panelManager = FindObjectOfType<PanelManager>();
     }
 
     void FixedUpdate()
     {
         if (gameManager.state == GameState.Play)
         {
-            movementNumber = PlayerPrefs.GetInt("MovementNumber");
-            switch (movementNumber)
-            {
-                case 1:
-                    if (Input.touchCount > 0)
+        movementNumber = PlayerPrefs.GetInt("MovementNumber");
+        switch (movementNumber)
+        {
+            case 1:
+                if (Input.touchCount > 0 && nowCanReplayPlayerMovment == 2)
+                {
+                    Touch touch = Input.GetTouch(0);
+
+                    if (touch.phase == TouchPhase.Began)
                     {
-                        Touch touch = Input.GetTouch(0);
-
-                        if (touch.phase == TouchPhase.Began)
-                        {
-                            initialTouchPosition = Camera.main.ScreenToWorldPoint(touch.position);
-                            initialTouchPosition.z = 0f;
-                            initialPlayerPosition = transform.position;
-                        }
-
-                        if (touch.phase == TouchPhase.Moved)
-                        {
-                            Vector3 currentTouchPosition = Camera.main.ScreenToWorldPoint(touch.position);
-                            currentTouchPosition.z = 0f;
-
-                            Vector3 movementDelta = currentTouchPosition - initialTouchPosition;
-                            transform.position = new Vector3(initialPlayerPosition.x + movementDelta.x, initialPlayerPosition.y, transform.position.z);
-                        }
-
-                        // Clamp to screen bounds
-                        float screenWidth = Camera.main.orthographicSize * Camera.main.aspect;
-                        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -screenWidth, screenWidth), transform.position.y, transform.position.z);
+                        //touch.position.y=0f;
+                        initialTouchPosition = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, mainPosition.y, Camera.main.transform.position.z - transform.position.z));
+                        initialTouchPosition.y = mainPosition.y;
+                        initialTouchPosition.z = 0f;
+                        initialPlayerPosition = mainPosition;
                     }
-                    break;
-                case 2:
-                    if (right && !left)
+
+                    if (touch.phase == TouchPhase.Moved)
                     {
-                        moveDirection = 4.5f;
-                        transform.Translate(moveDirection * Time.deltaTime, 0, 0);
+                        Vector3 currentTouchPosition = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, mainPosition.y, Camera.main.transform.position.z - transform.position.z));
+                        currentTouchPosition.y = mainPosition.y;
+                        currentTouchPosition.z = 0f;
+
+                        Vector3 movementDelta = currentTouchPosition - initialTouchPosition;
+                        transform.position = new Vector3(initialPlayerPosition.x + movementDelta.x, initialPlayerPosition.y, transform.position.z);
                     }
-                    else if (left && !right)
-                    {
-                        moveDirection = -4.5f;
-                        transform.Translate(moveDirection * Time.deltaTime, 0, 0);
-                    }
-                    break;
-                case 3:
+
+                    // Clamp to screen bounds
+                    float screenWidth = Camera.main.orthographicSize * Camera.main.aspect;
+                    transform.position = new Vector3(Mathf.Clamp(transform.position.x, -screenWidth, screenWidth), mainPosition.y, transform.position.z);
+                }
+                break;
+            case 2:
+                if (right && !left)
+                {
+                    moveDirection = 4.5f;
+                    transform.Translate(moveDirection * Time.deltaTime, 0, 0);
+                }
+                else if (left && !right)
+                {
+                    moveDirection = -4.5f;
+                    transform.Translate(moveDirection * Time.deltaTime, 0, 0);
+                }
+                break;
+            case 3:
+                if (gameManager.state == GameState.Play)
+                {
                     float h = Input.acceleration.x;
                     transform.Translate(h * 5 * Time.deltaTime, 0, 0);
-                    break;
+                }
+                break;
 
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(setting.playerMovementNumber), setting.playerMovementNumber, null);
-            }
+            default:
+                throw new ArgumentOutOfRangeException(nameof(setting.playerMovementNumber), setting.playerMovementNumber, null);
+        }
         }
         else if (gameManager.state == GameState.Lose)
         {
@@ -98,5 +107,13 @@ public class PlayerMovement : MonoBehaviour
     public void Left_U()
     {
         left = false;
+    }
+
+    public void NewPlayerAnimEndPlayerMovment(int intAnim)
+    {
+        if (intAnim == 1)
+        {
+            nowCanReplayPlayerMovment = 2;
+        }
     }
 }
